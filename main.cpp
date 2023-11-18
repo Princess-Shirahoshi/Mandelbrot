0125ff92c505f030e7a7853e601d43413227ce1a
Your Name <you@example.com>
2023-11-18 15:03:45 -0800

    Window testing

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/System/Clock.hpp>
-#include <VideoMode.hpp>
+#include <SFML/Window/VideoMode.hpp>
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -14,3 +14,16 @@
 using namespace sf;
 using namespace std;
 
+unsigned int screenWidth;
+unsigned int screenHeight;
+//Main starting here
+
+int main()
+{
+    //Get the Desktop Resolution here && divide it by 2 to get the resolution
+    screenWidth = VideoMode::getDesktopMode().width / 2;
+    screenHeight = VideoMode::getDesktopMode().height / 2;
+
+    VideoMode desktop = VideoMode::getDesktopMode();
+    window.create(screenWidth, screenHeight)
+}

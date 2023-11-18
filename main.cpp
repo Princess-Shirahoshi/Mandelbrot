ba8d3757734fdce8d957063c0e01fc365ae30d99
Your Name <you@example.com>
2023-11-18 14:37:45 -0800

    Added Headers && Namespace ~needed <VideoMode.hpp> to get resolution~

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Extra Credit if we do threading 
+// https://www.sfml-dev.org/tutorials/2.5/system-thread.php
+
+//Headers
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System/Clock.hpp>
+#include <VideoMode.hpp>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+//Namespace Declarations 
+using namespace sf;
+using namespace std;
+

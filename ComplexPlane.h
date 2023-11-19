5fb297783266b7733aa5c5645e19818b117ac308
Your Name <you@example.com>
2023-11-18 21:58:38 -0800

    Updated header

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -14,10 +14,12 @@ class ComplexPlane : public Drawable
         const float BASE_WIDTH = 4.0;
         const float BASE_HEIGHT = 4.0;
         const float BASE_ZOOM = 0.5;
-        
+
         enum class State
         {
             CALCULATING,
             DISPLAYING
         };
-}
+};
+
+#endif 
